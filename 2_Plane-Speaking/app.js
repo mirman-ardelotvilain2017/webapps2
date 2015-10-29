9498bcb835517394d24d62de65dc7e694579193d
Arthur DelotVilain <ardelotvilain@ardelotvilain2017.local>
2015-10-29 15:15:00 -0700

    hey

--- 2_Plane-Speaking/app.js
+++ 2_Plane-Speaking/app.js
@@ -18,7 +18,7 @@ var xPlanes = [
     "name":"X-29",
     "description":"Wings jut out forwards",
     "imageURL":"https://upload.wikimedia.org/wikipedia/commons/9/91/Grumman-X29-InFlight.jpg"
-  }
+  },
   {
     "name":"X-31",
     "description":"Test plane for supermaneuverability",
@@ -30,10 +30,10 @@ var xPlanes = [
     "imageURL":"http://galerie.valka.cz/data/962/medium/X-37A_001.jpg"
   },
   {
-    "name":"X-45"
+    "name":"X-45",
     "description":"UCAV (Unmanned Combat Air Vehicle) Tester",
     "imageURL":"https://upload.wikimedia.org/wikipedia/commons/2/2a/US_Navy_050627-N-0295M-003_A_full_scale_Joint_Unmanned_Combat_Air_Systems_(J-UCAS)_X-45C_on_display_at_the_2005_Naval_Unmanned_Aerial_Vehicle_Air_Demo_held_at_the_Webster_Field_Annex_of_Naval_Air_Station_Patuxent_River.jpg"
-  }
+  },
 ];
 
 // Now that we have our array of planes, we need to add elements to our DOM
@@ -42,6 +42,8 @@ var xPlanes = [
 var RickPasta = document.getElementById("planes-list"); //This line is unfinished! Use a method to capture our <ul> element.
 
 for(var plane in xPlanes) {
-  var JohnPasta = xPlanes[i]
-  JohnPasta.innerHTML += "<h2>" + xPlane.name+"</h2>"
+  var JohnPasta = xPlanes[plane];
+  RickPasta.innerHTML += "<h2>" + JohnPasta.name+"</h2>";
+  RickPasta.innerHTML += "<p>" + JohnPasta.description+"</p>";
+  RickPasta.innerHTML += "<img src='" + JohnPasta.imageURL+ " ' />";
 }
